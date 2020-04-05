abb81ad0e790e4fc77b7061e7d4d6594f70afa9e
AlexSikand <alex@sikand.org>
2020-04-05 04:11:08 -0400

    Initial Commit

---
+++ TravelApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TravelApp
+//
+//  Created by Alex Sikand on 4/5/20.
+//  Copyright © 2020 AlexSikandEngineering. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
